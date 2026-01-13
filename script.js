564a7b7b5309b95f47c89ebc387d0467216364b2
LarsMartin-Lillerovde <239625292+dalsfjord-blip@users.noreply.github.com>
2026-01-13 13:08:23 +0100

    Saving halfway through.

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
-console.log("Hi")
+/*console.log("Hi")*/
 
-const options = ["rock", "paper", "scissors"];
+const options = ["rock","paper", "scissors"]
+
+function getComputerChoice(){
+    const choice = options[Math.floor(Math.random() * options.length)];
+    return choice;
+}
+
+function checkWinner(playerSelection, computerSelection){
+    if(playerSelection == computerSelection){
+        return "Tie";
+    }
+    else if(
+    (playerSelection == "rock" && computerSelection == "scissors") ||
+      (playerSelection == "scissors" && computerSelection == "paper")||
+      (playerSelection == "paper" && computerSelection == "rock")  
+    ){
+        return "Player";
+    }
+    else {
+        return "Computer";
+    }
+}
+
+function playRound(playerSelection, computerSelection){
+
+}
+
+getComputerChoice();
